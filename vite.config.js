c2415ddac169210602eb2c146f3950eeffcc524c
Aditya Patil <addi1402@gmail.com>
2025-02-08 10:41:27 +0530

    config(indentation): set to 2 spaces

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { path } from 'path';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'@styles': path.resolve('./src/styles'),
-			'@icons': path.resolve('./src/icons'),
-			'@components': path.resolve('./src/lib'),
-			'@animations': path.resolve('./src/lib/animations')
-		}
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve('./src/styles/'),
+      '@icons': path.resolve('./src/icons/'),
+      '@components': path.resolve('./src/lib/'),
+      '@animations': path.resolve('./src/lib/animations/')
+    }
+  }
 });
